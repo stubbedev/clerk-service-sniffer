04759b8ddab2a66d0222c4b945bed4eca2d4e6c3
braxclerk <brp@clerk.io>
2024-02-26 16:48:51 +0100

    Updated styling, logo's, main Clerk logo, stopped error(s) with syntax problems

--- js/popup.js
+++ js/popup.js
@@ -6,8 +6,14 @@ document.addEventListener("DOMContentLoaded", async function() {
     const data = await handleStorage(null, tab.id, null);
     updateDOM(data)
     
-    updateLogos(data.competitors);  //Unsure what properties to put in here
-    console.log("Fetched Competitor Data *****:", data.competitors);
+  if (data && data.competitors) {
+    updateLogos(data.competitors);
+      } else {
+      console.error('Data is undefined or does not have a competitors property', error);
+    }
+    
+
+   // updateLogos(data.competitors);  //Unsure what properties to put in here
 
   }
 })
@@ -27,24 +33,6 @@ async function handlePopup(request, sender) {
   return true;
 }
 
-//Cant get it to work or log any of the console.log's below 
-
-// // This event listener ensures the popup updates when the active tab changes
-// chrome.tabs.onUpdated.addListener(function(tabId, tab) {
-//   console.log("Tab Update Detected:", tabId);
-//   if (changeInfo.status === 'complete' && tab.active) {
-//     refreshPopupData(tabId);
-//   }
-// });
-
-// // This function fetches new data for the given tabId and updates the DOM
-// async function refreshPopupData(tabId) {
-//   console.log("Refreshing data for tab:", tabId);
-//   const newData = await handleStorage(null, tabId, null);
-//   console.log("New data fetched:", data);
-//   updateDOM(newData);
-// }
-
 
 function updateDOM(data) {
   const comps = data?.competitors;
@@ -58,33 +46,20 @@ function updateDOM(data) {
     target.append(createListEntry(comps[i]));
   }
 
+  // Change main logo to clerk logo 
   if (compFound) {
-    const imgEl = document.querySelector("#logoImage");
-    const headingEl = document.querySelector('.heading h2')
-    imgEl.src = `..${comps[0].icons["48"]}`;
-    headingEl.innerHTML = `<span>Service Used:</span>&nbsp;<b>${comps[0].name}</b>`;
+    const imgEl = document.querySelector('.heading img');
+    const headingEl = document.querySelector('.heading h2'); // Moved this line before its first use.
+    if (headingEl){
+      // If imgEl is supposed to change, uncomment the next line
+      // imgEl.src = `..${comps[0].icons["48"]}`;
+      headingEl.innerHTML = `<span>Service Used:</span>&nbsp;<b>${comps[0].name}</b>`;
+    } else {
+      console.error('The heading element was not found in the DOM');
+    }
   }
 }
 
-// //Tried to get it to refresh automatically each time the extension is opened 
-// // Function to refresh popup data on open of popup
-// async function refreshPopupDataOnOpen() {
-//   const tab = await getCurrentTab();
-//   if (tab) {
-//     const data = await handleStorage(null, tab.id, null);
-
-//     updateDOM(data);
-//   }
-// }
-// // doing an onMessage.addListener so we should be using 'request' as the variable in the console.log? 
-// // Event listener for when the popup is opened
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   console.log("Popup opened*, recieving request:", request);
-//   //unsure about 82-83
-//   if (request.message.status === true) {
-//     refreshPopupDataOnOpen();
-//   }
-// });
 
 
 function createListEntry(comp) {
@@ -93,6 +68,10 @@ function createListEntry(comp) {
   el.dataset.icons = JSON.stringify(comp.icons);
   el.dataset.detectedOn = comp.detected_on;
   el.className = "compEntry";
+  const icon = document.createElement('img');
+  icon.src = comp.icons['48']
+  icon.className = "entryIcon"; 
+  el.append(icon); 
   const name = document.createElement('div')
   name.className = "entryTitle"
   name.innerHTML = `<h4>Name: ${comp.name}</h4><img src="${comp.icons['32']}"/>`
@@ -111,7 +90,6 @@ function createListEntry(comp) {
 }
 
 function updateLogos(competitors){
-  console.log("Isarray?", Array.isArray(competitors));  //Doesnt work 
   const logoContainer = document.getElementById('logo-container'); 
   logoContainer.innerHTML = ''; 
 
@@ -130,32 +108,18 @@ function updateLogos(competitors){
 }
 
 
-
-
-//Preserve if URL is the same but tab is different del
-
 async function handleStorage(data, tabId, action) {
-  console.log("Stored data before processing", data); //
-  console.log("New data for current tab:", tabId); //
-
 //We are creating tabData list and populating it with data from tabData[tabId] and then equal it / add to data with '=' 
   const tabData = {};
   if (data) {
     tabData[tabId] = data;
   }
-
   const storedData = await chrome.storage.session.get({ data: {} });
-  console.log("Stored data before processing", data); //
-
-  console.log("New data for new tab:", tabData); //
 
   // if(storedData[tabId]['detected_on'] == data['detected_on']){
-
   // We ='ed 'detected_on' to the local window.origin , where origin is current web pages original link
 
     if (storedData[tabId] && storedData[tabId]['detected_on'] === data['detected_on']) {
-
-    // Add data as more than 1 competitor present 
     console.log("Checking comp", comp);
     //Accessing our storage of competitors based on tabID and data.competitors refers to the new array of competitors most recently fetched 
     //Making them = , means we are aware of any updates in tabId and competitor data 
@@ -165,10 +129,6 @@ async function handleStorage(data, tabId, action) {
     console.log("Current tab data:", storedData[tabId]); // 
     console.log("New data:", data); // 
 
-    // none of the below logged 
-    // console.log("Competitors before merge:", storedData[tabId].competitors, data.competitors);
-    // storedData[tabId].competitors = [...storedData[tabId].competitors, ...data.competitors];
-    // console.log("Competitors after merge:", storedData[tabId].competitors);
 
   } else {
     // wipe data currently saved in sessionStorage for tabId and save new data under that tabId
@@ -177,30 +137,17 @@ async function handleStorage(data, tabId, action) {
 
     storedData[tabId] = data;
 
-    console.log("Replacing old data with new data for tab", tabId); //
-    console.log("Checking tabData", tabData); //
-    console.log("Checking data", data); //null 
   }
-
   //creating an object (tmpdata) by copying stored.Data & Tabdata into it 
+
+  ///CHECK FOR tmpData before ATTEMPTING TO DELETE (DONE)
   const tmpData = Object.assign({}, storedData.data, tabData);
-
-    
-    console.log("tmp Data Object **:", tmpData); //
-    console.log("Action received, CHECK:", action);  // NULL
-
-  
   if (action === "delete" && tmpData[tabId]) {
     delete tmpData[tabId];
 
   }
   await chrome.storage.session.set({ data: tmpData });
-  
-
   console.log("Does tmpData for tabId exist?", Boolean(tmpData[tabId])); //
-
-
-
   return tmpData[tabId];
 
 
@@ -212,7 +159,7 @@ chrome.tabs.onRemoved.addListener(async function(tabid, removed) {
 });
 
 chrome.runtime.onMessage.addListener(
-  async function(request, sender, sendResponse) {
+  async function(request, sender, sendRespons) {
     if (request.source == "CleSS" && request.type == "popup") {
       await handlePopup(request, sender);
     }
